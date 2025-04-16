11afe5764c8acee889ba2d088b1deab2e0db1d53
DJ-BoT07 <djmangaonkar7@gmail.com>
2025-04-16 14:30:03 +0530

    New Logo 3

--- server/main.py
+++ server/main.py
@@ -143,5 +143,5 @@ def process_file_and_matrix():
     return jsonify({"co_values": co_values, "po_values": po_values})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000))))
 
